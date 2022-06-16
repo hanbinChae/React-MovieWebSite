c8dbcddf9bd5547ced477ff6a81da1101591475f
hanbinChae <hanbin2043@gmail.com>
2022-06-16 17:15:00 +0900

    6/16

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -4,14 +4,28 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword,setKeyword] = useState("");
+
   const onClick=()=>setValue((prev)=>prev+1);
-  console.log("i run all the time")
-  const iRunOnlyOnce = () =>{
-    console.log("CALL THE API.....");
+  const onChange = (event) =>{
+    setKeyword(event.target.value);
   }
-  useEffect(iRunOnlyOnce, []);
+  
+  useEffect(()=>{
+    console.log("I run only once");
+  }, []);
+  useEffect(()=>{
+    if (keyword !=="" && keyword.length>5 ){
+      console.log("I run when 'keyword' changes.");  
+    }
+  },[keyword])
+  useEffect(()=>{
+      console.log("I run when 'counter' changes.");  
+  },[counter])
+
   return (
     <div>
+      <input value={keyword} onChange={onChange} type='text' placeholder="Search here..."/>
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>Click me</button>
       <Button text={"Continue"}/>
