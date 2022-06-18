6f0e1678589f6e1c3176f8578c8dc9900703932e
hanbinChae <hanbin2043@gmail.com>
2022-06-18 19:05:31 +0900

    cleanup function

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -5,12 +5,21 @@ import { useState, useEffect } from "react";
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword,setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
 
   const onClick=()=>setValue((prev)=>prev+1);
   const onChange = (event) =>{
     setKeyword(event.target.value);
   }
-   
+  const onClick2=()=>setShowing(prev => !prev);
+
+  function Hello(){
+    useEffect(()=>{
+     console.log("created!");
+     return () => console.log("destroyed!");
+    },[])
+    return <h2>Hello</h2>;
+  }
   useEffect(()=>{
     console.log("I run only once");
   }, []);
@@ -27,6 +36,8 @@ function App() {
     <div>
       <input value={keyword} onChange={onChange} type='text' placeholder="Search here..."/>
       <h1 className={styles.title}>{counter}</h1>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
       <button onClick={onClick}>Click me</button>
       <Button text={"Continue"}/>
     </div>
