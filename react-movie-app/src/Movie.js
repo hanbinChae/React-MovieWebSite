45f8993c0311686e033f973c521255c8b7d028df
hanbinChae <hanbin2043@gmail.com>
2022-06-28 21:04:48 +0900

    show text

--- react-movie-app/src/Movie.js
+++ react-movie-app/src/Movie.js
@@ -2,16 +2,38 @@ import { useState,useEffect } from "react";
 
 function Movie(){
     const [loading, setLoading] = useState(true);
+    const [movies , setMovies] = useState([]);
+    const getMoives = async() => {
+        const json = await (
+            await fetch(
+                `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+            )
+        ).json();
+                
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
+
     useEffect(()=>{
-        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+        getMoives()
     },[]);
 
-    return (<div>
-        {loading ? <h1>Loading...</h1> : null}
-
-    </div>);
-}
+{/* main */}
+    return (
+        <div>
+            {loading ? (<h1>Loading...</h1>) 
+                : 
+            (<div>
+                {movies.map(movie => 
+                <div key={movie.id}>
+                    <h2>{movie.title}</h2>
+                    <p>{movie.summary}</p>
+                    <ul>
+                        {movie.genres.map(g => <li key={g}>{g}</li>)}
+                    </ul>
+                </div>)}
+            </div>)}      
+        </div>
+    )}
 
 export default Movie;
