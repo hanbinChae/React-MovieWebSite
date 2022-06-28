3bff82ca4e6820aec1b2b6cf22e447e03de3240a
hanbinChae <hanbin2043@gmail.com>
2022-06-28 20:35:42 +0900

    movie one step

---
+++ react-movie-app/src/Movie.js
@@ -0,0 +1,17 @@
+import { useState,useEffect } from "react";
+
+function Movie(){
+    const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    },[]);
+
+    return (<div>
+        {loading ? <h1>Loading...</h1> : null}
+
+    </div>);
+}
+
+export default Movie;
--- react-movie-app/src/index.js
+++ react-movie-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Movie from './Movie';
 import TodoList from './TodoList';
 import CoinTracker from './CoinTracker';
 
@@ -7,7 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<TodoList/> Todo list */}
-    <CoinTracker />
+    {/*<CoinTracker /> Todo list */}
+    <Movie/>
   </React.StrictMode>
 );
 
