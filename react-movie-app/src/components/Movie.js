35ec4097025ff249e266531505b21453cf5c1962
hanbinChae <hanbin2043@gmail.com>
2022-07-11 16:14:23 +0900

    7/11

--- react-movie-app/src/components/Movie.js
+++ react-movie-app/src/components/Movie.js
@@ -1,17 +1,6 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
 function Movie({coverImg,title,summary,genres}){
-    const [loading, setLoading] = useState(true);
-    const [movies , setMovies] = useState([]);
-    const getMoives = async() => {
-        const json = await (
-            await fetch(
-                `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-            )
-        ).json();
-                
-        setMovies(json.data.movies);
-        setLoading(false);
     return (
     <div>                
         <img src={coverImg} alt={title}></img>
@@ -24,14 +13,13 @@ function Movie({coverImg,title,summary,genres}){
         </ul>
     </div>
     );
-    }
 }
 
-Movie.PropTypes = {
+Movie.propTypes = {
     coverImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     genres: PropTypes.arrayOf(PropTypes.string).isRequired
-};
+}
 
 export default Movie;
--- react-movie-app/src/index.js
+++ react-movie-app/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import TodoList from './TodoList';
-import CoinTracker from './CoinTracker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<TodoList/> Todo list */}
-    {/*<CoinTracker /> Todo list */}
     <App/>
   </React.StrictMode>
 );
